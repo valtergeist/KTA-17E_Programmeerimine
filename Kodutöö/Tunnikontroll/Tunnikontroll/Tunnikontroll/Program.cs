ce8199b357aa652447c0bdade73ca38629bcfb31
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-11-13 11:46:06 +0200

    commit2
    loogika muutus

--- Kodutöö/Tunnikontroll/Tunnikontroll/Tunnikontroll/Program.cs
+++ Kodutöö/Tunnikontroll/Tunnikontroll/Tunnikontroll/Program.cs
@@ -14,6 +14,8 @@ namespace Tunnikontroll
             double sisend1;
             double tavaklient;
             double pysiklient;
+            double Pmakse;
+            double Tmakse; 
 
 
             sisend1 = SetNumber("Kirjuta siia ostusumma: \n");
@@ -21,27 +23,33 @@ namespace Tunnikontroll
             if (sisend1 < 250 && sisend1 >= 50)
             {
                 tavaklient = (sisend1 / 100 * 10);
+                Tmakse = sisend1 - tavaklient;
                 pysiklient = (sisend1 / 100 * 20);
+                Pmakse = sisend1 - pysiklient;
                 Console.WriteLine($"Tavakliendi allahindlus on 10% ja püsikliendi allahindlus on 20%");
-                Console.WriteLine($"Tasuda on vaja tavakliendil {tavaklient} eurot  püsikliendil {pysiklient} eurot ");
+                Console.WriteLine($"Tasuda on vaja tavakliendil {Tmakse} eurot  püsikliendil {Pmakse} eurot ");
                 Console.ReadKey();
             }
 
             else if (sisend1 < 350 && sisend1 >= 250)
             {
                 tavaklient = (sisend1 / 100 * 20);
+                Tmakse = sisend1 - tavaklient;
                 pysiklient = (sisend1 / 100 * 30);
+                Pmakse = sisend1 - pysiklient;
                 Console.WriteLine($"Tavakliendi allahindlus on 20% ja püsikliendi allahindlus on 30%");
-                Console.WriteLine($"Tasuda on vaja tavakliendil {tavaklient} eurot  püsikliendil {pysiklient} eurot");
+                Console.WriteLine($"Tasuda on vaja tavakliendil {Tmakse} eurot  püsikliendil {Pmakse} eurot ");
                 Console.ReadKey();
             }
 
             else if (sisend1 >= 350)
             {
                 tavaklient = (sisend1 / 100 * 30);
+                Tmakse = sisend1 - tavaklient;
                 pysiklient = (sisend1 / 100 * 40);
+                Pmakse = sisend1 - pysiklient;
                 Console.WriteLine($"Tavakliendi allahindlus on 30% ja püsikliendi allahindlus on 40%");
-                Console.WriteLine($"Tasuda on vaja tavakliendil {tavaklient} eurot püsikliendil {pysiklient} eurot");
+                Console.WriteLine($"Tasuda on vaja tavakliendil {Tmakse} eurot  püsikliendil {Pmakse} eurot ");
                 Console.ReadKey();
             }
 

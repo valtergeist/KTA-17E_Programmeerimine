8856a90e350bcda05b15e31c570d96edb83d812f
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-11-13 14:16:42 +0200

    6x6

---
+++ Excrecises/6x6/6x6table/6x6table/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6x6table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 6; i++)
+            {
+                Console.Write(i +"\t");
+
+                for (int j = 1; j <= 6; j++)
+                {
+                    if (i > 0)
+                    {
+                        Console.Write(i * j + "\t");
+                    }
+                    else
+                    {
+                        Console.Write(j + "\t");
+                    }
+                }
+                Console.Write("\n");
+ 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Excrecises/6x6multitable/6x6table/6x6table/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6x6table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------------");
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write(" " + i);
+                for (int j = 0; j < 7; j++)
+                    {
+                        if (i > 0)
+                        Console.WriteLine($"{i*j}|{i * j}|{i * j}|{i * j}|{i * j}|{i * j}");
+                    }
+
+
+         
+                Console.ReadKey();
+            }
+            Console.WriteLine("----------------------------------");
+        }
+    }
+}
